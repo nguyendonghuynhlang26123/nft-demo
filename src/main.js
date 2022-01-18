5f34d747d1d15225093a10ed49b9188eaa8f9151
Nguyen Dang Huynh Long <ldh@elcaNet.local>
2022-01-18 14:46:23 +0700

    update get by string

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ const port = process.env.PORT || 4000;
 
 app.get('/tokenuri/:id', (req, res) => {
   const id = req.params.id;
-  if (isNaN(id)) return res.status(400).json({ message: 'Id must be a number' });
-  const ticket = db.find((t) => t.ticket_id === Number(id));
+  const ticket = db.find((t) => t.ticket_id === id);
   if (!ticket) return res.status(404).json({ message: 'Not found ticket' });
 
   return res.status(200).json(ticket);
